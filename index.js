92a9b694301fcd15cce5a01b593aa735f798106b
Minhaz1901 <md.minhazul.islam1@g.bracu.ac.bd>
2023-08-15 17:44:23 +0600

    delete classes added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-//const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 //const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000;
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ib8fgeo.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,13 +29,80 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("FluentLink").collection("users");
     const courseCollection = client.db("FluentLink").collection("courses");
+    const selectedClassCollection = client.db("FluentLink").collection("selectedClasses");
 
+    // Courses collection apis
     app.get('/courses', async (req, res) => {
         const result = await courseCollection.find().toArray();
         res.send(result);
       })
     
+    // user collection apis
+    app.get('/users',  async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ insertedId: true })
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+
+
+    // selectedClasses collection apis
+    app.get('/selectedClasses', async (req, res) => {
+    const email = req.query.email;
+
+    if (!email) {
+        res.send([]);
+    }
+
+    // const decodedEmail = req.decoded.email;
+    // if (email !== decodedEmail) {
+    //     return res.status(403).send({ error: true, message: 'forbidden access' })
+    // }
+
+    const query = { email: email };
+    const result = await selectedClassCollection.find(query).toArray();
+    res.send(result);
+    });
+    
+    app.post('/selectedClasses', async (req, res) => {
+        const item = req.body;
+        const result = await selectedClassCollection.insertOne(item);
+        res.send(result);
+      });
+
+    app.delete('/selectedClasses/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await selectedClassCollection.deleteOne(query);
+      res.send(result);
+    });
 
 
     // Send a ping to confirm a successful connection
